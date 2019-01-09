f2eaa8325071207a0076de2ddff4410fd3fb7319
Yago Rogatto Leme <yaguetto@Yagos-Macbook.local>
2019-01-09 21:03:39 -0200

    gerando o baralho automaticamente

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 
 def main():
-    print('♣')
+    baralho = criar_baralho()
+    print('baralho criado')
+
+
+    # ♠ ♣ ♥ ♦
+def criar_baralho():
+    baralho_real = []
+    letras_baralho = ['A', 'J', 'Q', 'K']
+    cont = 0
+    for each in letras_baralho:
+        x = str(letras_baralho[cont])+'♠'
+        baralho_real.append(x)
+        x = str(letras_baralho[cont])+'♣'
+        baralho_real.append(x)
+        x = str(letras_baralho[cont])+'♥'
+        baralho_real.append(x)
+        x = str(letras_baralho[cont])+'♦'
+        baralho_real.append(x)
+        cont = cont + 1
+    for each in range(2, 11):
+        x = str(each)+'♠'
+        baralho_real.append(x)
+        x = str(each)+'♣'
+        baralho_real.append(x)
+        x = str(each)+'♥'
+        baralho_real.append(x)
+        x = str(each)+'♦'
+        baralho_real.append(x)
+        cont = cont + 1
+    return baralho_real
 
 if __name__ == '__main__':
     main()
